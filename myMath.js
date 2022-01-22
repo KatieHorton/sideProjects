7f95fa328419d038816aa0950c3aac119121da0a
Katie Horton <65191330+KatieHorton@users.noreply.github.com>
2022-01-22 15:27:02 -0600

    update myMath

--- myMath.js
+++ myMath.js
@@ -1,3 +1,15 @@
+// VARIABLES
+
+
+const pi = 3.141592653589793238462643383279502383197169399375105820974944592307816406286208998628034825342117067;
+const lightSpeed = 299792458; // meters/second
+const waterBoils = 212;  // degrees F
+const waterFreezes = 32;  // degrees F
+const earthRadius = 6378.1; // km 
+const moonRadius = 6786 // mi
+const sunRadius = 2.720984 // million mi
+const temperatureSun = 5778 // K
+
 // FORMULAS
 
 // mean
@@ -21,7 +33,13 @@ function median(numbers) {
 // console.log(median([1, 2, 3, 4, 5, 6, 7])); // 4
 // console.log(median([2, 3, 4, 5])); // 3
 
-let pi = 3.141592653589793238462643383279502383197169399375105820974944592307816406286208998628034825342117067;
+
+// mode
+function mode(numbers) {
+ //reduce to return most frequently occoring value in numbers?
+
+}
+
 
 //  basic counter
 let total = 0, count = 1;
@@ -84,7 +102,7 @@ return `Midpoint: ( ${x/2}, ${y/2} )`
 
 // area of triangle
 
-const areaTriangle = (base, height) => `${(1/2) * base * height} square units`;
+const areaTriangle = (base, height) => `${(1/2) * base * height}: square units`;
 // console.log(areaTriangle(2, 4)); // 4
  
 // Pythagoreon Theorem: 
@@ -124,6 +142,8 @@ function areaCircle(radius) {
 function circumference(radius) {
     return `${Math.PI * 2 * radius}`
 }
+
+//console.log(circumference())
 // console.log(circumference(7)); // 43.9822
 // console.log(circumference(1079.6)); // 6783 mi
 
@@ -166,6 +186,7 @@ function fahrToCelc(fahrenheit) {
     return `${celcius} degrees C`
 }
 
+// console.log(fahrToCelc(waterFreezes)) // 0
 // console.log(fahrToCelc(86));  // 30
 // console.log(fahrToCelc(350)); // 176.6667
 
@@ -184,7 +205,7 @@ function kelvToFahr(kelvin) {
     let fahrenheit = ((kelvin - 273.15) * (9 / 5)) + 32;
     return `${fahrenheit} degrees F`
 }
-
+// console.log(kelvToFahr(temperatureSun)); // 9740.73 degrees F
 // console.log(kelvToFahr(500)); // 440.33
 // console.log(kelvToFahr(5778)); // 9940 F  
 
@@ -234,88 +255,88 @@ function sum([...numbers]) {
 // Distance:
 
 // find centimeters
-const inchesToCentimeters = (inches) => `${inches * 2.5400} centimeters`;
+const inchesToCentimeters = (inches) => `${inches * 2.5400}: 'centimeters'`;
 // console.log(inchesToCentimeters(12)); // 30.48
 
 // find meters
-const feetToMeters = (feet) => `${feet * 0.3048} meters`;
+const feetToMeters = (feet) => `${feet * 0.3048}: 'meters'`;
 // console.log(feetToMeters(2)); // 0.6096
 
-const yardsToMeters = (yards) => `${yards * 0.9144} meters`;
+const yardsToMeters = (yards) => `${yards * 0.9144}: 'meters'`;
 // console.log(yardsToMeters(5)); //4.572
 
 // find kilometers
-const milesToKilometers = (miles) => `${miles * 1.6093} kilometers`;
+const milesToKilometers = (miles) => `${miles * 1.6093}: 'kilometers'`;
 // console.log(milesToKilometers(17)); // 27.3581
 
 // find inches
-const millimetersToInches = (millimeters) => `${millimeters * 0.0394} inches`;
+const millimetersToInches = (millimeters) => `${millimeters * 0.0394}: 'inches'`;
 // console.log(millimetersToInches(150)); // 5.9099
 
-const centimetersToInches = (centimeters) => `${centimeters * 0.3937} inches`;
+const centimetersToInches = (centimeters) => `${centimeters * 0.3937}: 'inches'`;
 // console.log(centimetersToInches(15)); // 5.9055
 
 // find feet
-const metersToFeet = (meters) => `${meters * 3.2808} feet`;
+const metersToFeet = (meters) => `${meters * 3.2808}: 'feet'`;
 // console.log(metersToFeet(3)); // 9.8424
 
 // find miles
-const kilometersToMiles = (kilometers) => `${kilometers * 0.6214} miles`;
+const kilometersToMiles = (kilometers) => kilometers * 0.6214;
 // console.log(kilometersToMiles(100)); // 62.1399
-
+// console.log(circumference(kilometersToMiles(earthRadius))); // aprox 24901 mi
 
 // Surface or area:
 
 // find acres
-const hectacresToAcres = (hectacres) => `${hectacres * 2.4710} acres`;
+const hectacresToAcres = (hectacres) => `${hectacres * 2.4710}: 'acres'`;
 // console.log(hectacresToAcres(17)); // 42.0070
 
 
 // Volume and Capacity:
 
 // find liters
-const pintsToLiters = (pints) => `${pints * 0.4732} liters`;
+const pintsToLiters = (pints) => `${pints * 0.4732}: 'liters'`;
 // console.log(pintsToLiters(50)); // 23.66
 
-const quartsToLiters = (quarts) => `${quarts * 0.9463} liters`;
+const quartsToLiters = (quarts) => `${quarts * 0.9463}: 'liters'`;
 // console.log(quartsToLiters(50)); // 47.3150
 
-const gallonsToLiters = (gallons) => `${gallons * 3.7853} liters`;
+const gallonsToLiters = (gallons) => `${gallons * 3.7853}: 'liters'`;
 // console.log(gallonsToLiters(50)); // 189.265
 
 // find pints
-const litersToPints = (liters) => `${liters * 2.1134} pints`;
+const litersToPints = (liters) => `${liters * 2.1134}: 'pints'`;
 // console.log(litersToPints(50)); // 105.67
 
 // find quarts
-const litersToQuarts = (liters) => `${liters * 1.0567} quarts`;
+const litersToQuarts = (liters) => `${liters * 1.0567}: 'quarts'`;
 // console.log(litersToQuarts(50)); // 52.835
 
 // find gallons
-const litersToGallons = (liters) => `${liters * 0.2642} gallons`;
+const litersToGallons = (liters) => `${liters * 0.2642}: 'gallons'`;
 // console.log(litersToGallons(50)); // 13.9999
 
 
 // Weight and Mass:
 
 // find grams
-const ouncesToGrams = (ounces) => `${ounces * 28.3495} grams`;
+const ouncesToGrams = (ounces) => `${ounces * 28.3495}:'grams'`;
 // console.log(ouncesToGrams(50)); // 1417.475
 
 // find kilograms
-const poundsToKilograms = (pounds) => `${pounds * 0.4536} kilograms`;
+const poundsToKilograms = (pounds) => `${pounds * 0.4536}: 'kilograms'`;
 // console.log(poundsToKilograms(50)); // 22.68
 
 // find metric tons
-const shortTonsToMetricTons = (shortTons) => `${shortTons * 0.9072} metric tons`;
+const shortTonsToMetricTons = (shortTons) => `${shortTons * 0.9072}: 'metric tons'`;
 // console.log(shortTonsToMetricTons(50)); // 45.36
 
 // find pounds
-const kilogramsToPounds = (kilograms) => `${kilograms * 2.2046} pounds`;
-// console.log(kilogramsToPounds(50)); // 110.23
+const kilogramsToPounds = (kilograms) => `${kilograms * 2.2046}: 'pounds'`;
+ console.log(kilogramsToPounds(50)); // 110.23
 
 // find short tons
-const metricTonsToShortTons = (metricTons) => `${metricTons * 1.1023} short tons`;
+const metricTonsToShortTons = (metricTons) => `${metricTons * 1.1023}: 'short tons'`;
 // console.log(metricTonsToShortTons(50)); // 55.115
 
 
@@ -345,8 +366,6 @@ function phi(table) {
      return (table[3] * table[0] - table[2] * table[1]) / Math.sqrt((table[2] + table[3]) * (table[0] + table[1]) * (table[1] + table[3]) * (table[0] + table[2]));
  }
  // console.log(phi([76, 9, 4, 1])); // 0.068599434
-
- 
 
 
  // see all the correlations:
